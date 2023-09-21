5c5914824f179a796bc35ed50c3825b7e0d2118e
Himanshu <himan.work422@gmail.com>
2023-09-21 15:43:59 +0530

    Fixes

--- src/store.js
+++ src/store.js
@@ -1,5 +1,4 @@
 import { legacy_createStore as createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux';
 import modalReducer from './reducers/modalReducer';
 
 const rootReducer = combineReducers({
